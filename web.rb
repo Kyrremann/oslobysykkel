7de574feafa44c286f527c8ef885da986da9f355
Kyrre Havik Eriksen <kyrremann@gmail.com>
2018-06-13 10:23:40 +0200

    Simple station view

--- web.rb
+++ web.rb
@@ -3,6 +3,11 @@ require 'sinatra'
 
 require_relative 'models/init'
 
+get '/:station_id' do |station_id|
+  station = Station.find(station_id: station_id)
+  haml(:station, locals: { station: station })
+end
+
 get '/' do
   haml(:index)
 end
