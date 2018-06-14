d79190f7a35ab1387d31353fedd53d8ee0dd8aeb
Kyrre Havik Eriksen <kyrremann@gmail.com>
2018-06-14 09:30:02 +0200

    A simple status endpoint site

--- web.rb
+++ web.rb
@@ -1,7 +1,25 @@
 require 'haml'
+require 'http'
+require 'json'
 require 'sinatra'
 
 require_relative 'models/init'
+
+unless ENV['RACK_ENV'] == 'production'
+  require 'dotenv/load'
+end
+
+get '/status' do
+  http = HTTP
+           .headers('Client-Identifier': "#{ENV['BYSYKKEL_TOKEN']}")
+  response = http.get("#{ENV['BYSYKKEL_URL']}/status")
+  if response.status.ok?
+    status = JSON.parse(response.to_s)['status']
+    haml(:status, locals: { status: status })
+  else
+    response
+  end
+end
 
 get '/:station_id' do |station_id|
   station = Station.find(station_id: station_id)
